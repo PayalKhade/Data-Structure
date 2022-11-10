ba76dde3a2465af83a71f9c83e5c1312fe283223
Payal Khade <payalkhade3003@gmail.com>
2022-11-10 21:23:35 +0530

    New Pattern added

--- Pattern-Printing/src/Pattern16.java
+++ Pattern-Printing/src/Pattern16.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Pattern16 {
+/*
+A
+BB
+CCC
+DDDD
+EEEEE
+ */
+public class Pattern16 {
+    {
+        int size =5;
+        for (int i =1;i<=size;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                char c=(char) (64+i);
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
 }
